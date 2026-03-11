378d28ab4a60596ed7a3d27eed3ad3347a29b5cc
Yash Keshari <Yashkeshari0408@gmail.com>
2026-03-11 15:09:12 +0530

    DB Connection and User Schema

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require("express");
 
 const app = express();
 
-/* middleware */
 app.use(express.json());
 
-/* routes */
-app.get("/health", (req, res) => {
-  res.send("API is running");
-});
+
+// app.get("/health", (req, res) => {
+//   res.send("API is running");
+// });
 
 module.exports = app;
 
-// app.listen(3000)
